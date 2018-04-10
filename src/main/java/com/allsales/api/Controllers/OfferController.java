e81d1c2c75519cee7eaa75433281e662130f8f4c
Ermin Šabotić <ermin@misijaweb.com>
2018-04-10 22:42:19 +0200

    offer controller and offer repo initial creation

--- src/main/java/com/allsales/api/Controllers/OfferController.java
+++ src/main/java/com/allsales/api/Controllers/OfferController.java
@@ -1,7 +1,61 @@
 package com.allsales.api.Controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.allsales.api.Models.Offer;
+import com.allsales.api.Repositories.OfferRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/offers")
 public class OfferController {
+
+    private OfferRepository offerRepository;
+
+    @Autowired
+    public OfferController(OfferRepository offerRepository){
+        this.offerRepository = offerRepository;
+    }
+
+
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    public ResponseEntity<Offer> create(@RequestBody Offer offer){
+
+        offerRepository.save(offer);
+
+        return new ResponseEntity<>(offer, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "delete", method = RequestMethod.DELETE)
+    public HttpStatus destroy(Long id){
+
+        offerRepository.deleteById(id);
+
+        return HttpStatus.OK;
+    }
+
+    @RequestMapping(value = "index", method = RequestMethod.POST)
+    public ResponseEntity<List<Offer>> index(){
+
+        List<Offer> offers = offerRepository.findAll();
+
+        return new ResponseEntity<>(offers, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "find/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Offer> find(@PathVariable Long id){
+
+        Offer offer = offerRepository.findOfferById(id);
+
+        return new ResponseEntity<>(offer, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "update", method = RequestMethod.PUT)
+    public ResponseEntity<Offer> update(@RequestBody Offer offer){
+
+        return new ResponseEntity<>(offer, HttpStatus.OK);
+    }
 }
--- src/main/java/com/allsales/api/Repositories/OfferRepository.java
+++ src/main/java/com/allsales/api/Repositories/OfferRepository.java
@@ -2,6 +2,11 @@ package com.allsales.api.Repositories;
 
 import com.allsales.api.Models.Offer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OfferRepository extends JpaRepository<Offer, Long> {
+
+    @Query("SELECT u FROM User AS u where u.id = :id")
+    Offer findOfferById(@Param("id") Long id);
 }
